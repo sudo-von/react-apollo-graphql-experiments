ac7740a8d9e504a14be84e3e85bf908fda500896
VoN <sudo.von.contact@gmail.com>
2021-09-13 00:16:24 -0500

    feat: :sparkles: Add components to show fetched data. Add styles.

--- src/screens/Github/Github.js
+++ src/screens/Github/Github.js
@@ -3,13 +3,40 @@ import React from 'react'
 import { graphql } from 'react-apollo'
 /* Services. */
 import { getRepositories } from '../../services/github-service'
+/* Atomize components. */
+import { Container, Row, Col, Div, Text } from 'atomize'
 
-const Github = (props) => {
-  console.log(props)
+const Github = ({ data }) => {
+
+  /* Destructuring data. */
+  const { loading, search } = data
+
   return (
-    <div>
-      Hello
-    </div>
+    <Container bg='info900' d='flex' flexDir='column' justify='center' align='center' minH={{ md: '100vh' }}>
+      <Row>
+        <Col size='12'>
+          { loading &&
+            <Div p='1rem' bg='white' rounded='sm' m='3rem' shadow='3'>
+              <Text tag='h1'>Loading repositories...</Text>
+            </Div>
+          }
+          {
+            search && search.edges.map(({node}) =>
+              <Div p='1rem' bg='white' rounded='sm' m='3rem' shadow='3'>
+                <Text tag='h1'>ID</Text>
+                <p>{node.id}</p>
+                <Text tag='h1'>Name</Text>
+                <p>{node.name}</p>
+                <Text tag='h1'>Description</Text>
+                <p>{node.description}</p>
+                <Text tag='h1'>URL</Text>
+                <p><a href={node.url} target='blank'>{node.url}</a></p>
+              </Div>
+            )
+          }
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
@@ -19,7 +46,7 @@ const GithubWithData = graphql(
   {
     options: {
       variables: {
-        name: 'von'
+        name: 'hacking'
       }
     }
   }
