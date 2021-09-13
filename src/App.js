dfd8253bb856914faae9e714e72e2a27627081ed
VoN <sudo.von.contact@gmail.com>
2021-09-12 23:37:58 -0500

    feat: :sparkles: Add styletron, atomize dependencies. Add styletron and atomize providers.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import { ApolloProvider } from 'react-apollo'
 import { client } from './helpers/apollo-helper'
 /* Screens. */
 import Github from './screens/Github'
+/* Styleron. */
+import { Provider as StyletronProvider, DebugEngine } from 'styletron-react'
+import { Client as Styletron } from 'styletron-engine-atomic'
+/* Atomize. */
+import { StyleReset } from 'atomize'
+
+const debug = process.env.NODE_ENV === "production" ? void 0 : new DebugEngine()
+  
+const engine = new Styletron()
 
 const App = () => (
   <ApolloProvider client={client}>
-    <Github/>
+    <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+      <StyleReset/>
+      <Github/>
+    </StyletronProvider>
   </ApolloProvider>
 )
 
