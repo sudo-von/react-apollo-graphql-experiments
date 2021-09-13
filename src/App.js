97dbe625e6286e8512868f35a1d26720cc1804ed
VoN <sudo.von.contact@gmail.com>
2021-09-12 23:28:01 -0500

    feat: :sparkles: Add client to apollo provider

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import React  from 'react'
 /* Apollo. */
 import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-
-const token = process.env.REACT_APP_TOKEN
-
-const httpLink = {
-  hri: 'https://api.github.com/graphql',
-  headers: {
-    authorization: `Bearer ${token}`
-  }
-}
-
-const client = new ApolloClient({
-  link: new HttpLink(httpLink),
-  cache: new InMemoryCache()
-})
+/* Helpers. */
+import { client } from './helpers/graphql-helper'
+/* Screens. */
+import Github from './screens/Github'
 
 const App = () => (
   <ApolloProvider client={client}>
-    <div>
-      asdsad
-    </div>
+    <Github/>
   </ApolloProvider>
 )
 
-export default App
+export default App 
